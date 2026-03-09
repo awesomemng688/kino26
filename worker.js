cb7428346330ee60624cf1682a1156745fb84afe
awesomemng688 <pingohatka@gmail.com>
2026-03-09 16:35:02 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,16 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
 
 addEventListener('fetch', event => {
-  event.respondWith(
-    getAssetFromKV(event)
-  )
-});
+  event.respondWith(handleEvent(event))
+})
+
+async function handleEvent(event) {
+  try {
+    // React-ийн static файлуудыг serve хийх
+    return await getAssetFromKV(event)
+  } catch (e) {
+    // React-ийн SPA-д зориулсан fallback
+    const index = await getAssetFromKV(event, { mapRequestToAsset: req => new Request('/index.html', req) })
+    return index
+  }
+}
